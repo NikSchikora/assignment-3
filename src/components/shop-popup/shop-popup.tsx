9fc0d3bf5d13a8057d3f35f2a02fce3f9673b129
linakaefer <57412747+linakaefer@users.noreply.github.com>
2020-04-29 19:03:15 +0200

    basic design was added

--- src/components/shop-popup/shop-popup.tsx
+++ src/components/shop-popup/shop-popup.tsx
@@ -9,33 +9,22 @@ export class ShopPopup implements ComponentInterface {
  @Method()
  async test() {
 
-  const verschwinden=document.getElementById("newsletter-popup")!;
-  const button2=document.getElementById("button2")!;
-  const button1=document.getElementById("button1")!;
-  
-  function hideItem() :void {
-   verschwinden.style.display="none";
-  }
-  
-  button2.addEventListener("click", hideItem);
-  
-  
-  
-  function passOn (): void {
-      window.location.href = ("https://play.google.com/store/apps?hl=de");
-  }
-  button1.addEventListener("click", passOn)
  }
  
   render() {
     return (
       <div id="newsletter-popup">
       <div class="hinweis">
-        <span>Willst du immer über die neusten Styles infomiert sein?</span>
-        <p>Abonniere unseren kostenlosen Newsletter und erhalte einen 5€ Gutschein. <br></br>
-        Außerdem erhälst du eine kostenlose Style-Beratung von unseren Experten!</p>
-        <button id="button1" >OK, I accept</button>
-			<button id="button2">Cancel</button>
+        <span>Sign up for our newsletter</span>  
+        <p>You also recieve a 5 € Voucher!</p>    
+        <div>
+         <img class ="news" src="news.png"> </img>
+         </div>
+          <form><p>
+              <label>Email-Adresse:</label>
+              <input id="email" required> </input>
+              <button type="submit">Send</button></p>
+          </form>
       </div>
     </div>
     );
