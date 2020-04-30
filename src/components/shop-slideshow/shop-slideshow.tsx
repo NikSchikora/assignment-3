6699e27ef30ac46eae2a273b82b69c257be2fd0e
Laura Krumm <krumm.laura98@gmail.com>
2020-04-30 11:00:27 +0200

    added import to write js into the component

--- src/components/shop-slideshow/shop-slideshow.tsx
+++ src/components/shop-slideshow/shop-slideshow.tsx
@@ -1,4 +1,4 @@
-import { Component, ComponentInterface, Host, h } from '@stencil/core';
+import { Component, ComponentInterface, Host, h, Method } from '@stencil/core';
 
 @Component({
   tag: 'shop-slideshow',
@@ -6,11 +6,40 @@ import { Component, ComponentInterface, Host, h } from '@stencil/core';
   shadow: true,
 })
 export class ShopSlideshow implements ComponentInterface {
+ @Method()
+
+ async slideshow() {
+
+  var index = 0;     
+  slideshow(); 
+
+  function slideshow() {  
+      var i;  
+      var x = document.getElementsByTagName("img");   
+      for (i = 0; i < x.length; i++) {    
+      x[i].style.display = "none";    
+      }
+      index++;   
+      if (index > x.length) {index = 1}   
+      x[index-1].style.display = "block";    
+      setTimeout(slideshow, 5000); 
+  }
+ }
 
   render() {
     return (
       <Host>
-        <slot></slot>
+        <div id="sidebar"><a href="#" class="image image-full">
+        <div class="slide">
+          <img src="images/pic05.jpg" alt="" />
+        </div>
+        <div class="slide">
+          <img src="images/pic04.jpg" alt="" />	
+        </div>
+        <div class="slide">
+          <img src="images/pic03.jpg" alt="" />	
+        </div>
+      </a></div>
       </Host>
     );
   }
