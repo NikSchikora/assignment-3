cb94afb648cbc8efb773133c815c1ab93be7d4e9
linakaefer <57412747+linakaefer@users.noreply.github.com>
2020-05-05 15:56:41 +0200

    function was added

--- src/components/shop-teaser/shop-teaser.tsx
+++ src/components/shop-teaser/shop-teaser.tsx
@@ -9,18 +9,29 @@ import {Prop, getAssetPath, Component, ComponentInterface, Host, h } from '@sten
 export class ShopTeaser implements ComponentInterface {
   @Prop() image="nähen.jpg";
 
+
+ passOn () {
+    window.location.href = ("https://play.google.com/store/apps?hl=de");
+}
+
   componentWillLoad() {
 alert("hallo");
+
+  }
+  componentDidLoad() {
+    document.getElementById("btnpassOn").addEventListener("onclick", this.passOn);
   }
   
   render() {
+ 
+    
     return (
       <article class="teaser">   
       <img src={getAssetPath(`./assets/${this.image}`)} />
     <h1><slot/></h1>      
 
     <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </p>
-    <button>read more</button>
+    <button id="btnpassOn">read more</button>
   </article>   
 
     );
